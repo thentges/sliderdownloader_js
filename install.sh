c7040e7b21c51f1de412ada6b1e2c37fbd82dd19
thentges <thibault.hentges@gmail.com>
2019-01-26 04:50:41 +0100

    install script

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# this line is to make sure the commands are available
+PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
+BASEDIR=$(dirname $0)
+
+echo 'this is the installation guide in order to configure the scripts'
+
+# prompt user for values
+read -p 'enter the location of your .txt containing tracks ::: ' txt_path
+read -p 'enter the path to your download directory  ::: ' download_dir
+read -p 'enter your mail provider according to this list https://nodemailer.com/smtp/well-known/ ::: ' mail_service
+read -p 'enter your (sender) email ::: ' mail_username
+read -sp 'enter your (sender) email password ::: ' mail_password
+echo
+read -p 'enter your mail (receiving) recaps ::: ' mail_recipient
+
+# delete the actual config.json if it exist
+if [ -f config.json ]; then
+    rm config.json
+fi
+
+# create the new config.json file with values
+echo "{
+    \"txt_path\": \"${txt_path}\",
+    \"download_dir\": \"${download_dir}\",
+    \"mail\": {
+        \"service\": \"${mail_service}\",
+        \"username\": \"${mail_username}\",
+        \"password\": \"${mail_password}\",
+        \"recipient\": \"${mail_recipient}\"
+}" >> config.json
+
+
+cd $BASEDIR && npm install && chmod +x ./script.sh
--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin
-npm start
+BASEDIR=$(dirname $0)
+cd $BASEDIR && npm start
